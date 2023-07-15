cdd94020434ee08c78dd84ade3cd00846282208b
AndriiKalinchuk <126283550+AndriiKalinchuk@users.noreply.github.com>
2023-07-15 14:54:54 +0300

    added functions + invokeAction

--- contacts.js
+++ contacts.js
@@ -1,20 +1,47 @@
 const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const contactsPath = "./contacts.json";
+const contactsPath = path.join(__dirname, "db/contacts.json");
+const updateContacts = async (contacts) =>
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код. Повертає масив контактів.
-}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+};
 
-function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+};
 
-function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту.
-}
+const addContact = async (data) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Wellcome to Node.js");
+// const { program } = require("commander");
+
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      console.log(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "remove":
+      const deletedContact = await removeContact(id);
+      console.log(deletedContact);
+      break;
+
+    case "add":
+      const newContact = await addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+
+    default:
+      console.log("Unknown action");
+  }
+};
+
+invokeAction({ action: "get", id: "rsKkOQUi80UsgVPCcLZZW" });
